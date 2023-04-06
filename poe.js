ae5297fc49860e662f899570919ae38cccebaeb2
孙占辉 <11895200+jdukwjdo@users.noreply.github.com>
2023-04-06 13:25:20 +0800

    Update poe.js

--- poe.js
+++ poe.js
@@ -1,6 +1,6 @@
 const requestBody = $request.body;
 const responseBody = $response.body;
 
-$notify("命中请求:", "", JSON.stringify($request.body.query));
+$notify("命中请求:", JSON.stringify($request.body.query), "");
 
 $done();
