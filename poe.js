da126d373218a3d0ff4b2cadd24ac00c814ccd8b
sunzhanhui1 <sunzhanhui1@jd.com>
2023-04-06 13:05:14 +0800

    判断条件

--- poe.js
+++ poe.js
@@ -3,5 +3,8 @@ $notify("hello, poeInQuanx!", "", ""); // 发送通知标题为 "hello, poeInQua
 const requestBody = JSON.stringify($request.body); // 将请求体转换成字符串
 const responseBody = JSON.stringify($response.body); // 将响应体转换成字符串
 
-$notify("请求", "", requestBody); // 发送通知标题为 "请求"，内容为请求体的通知
-$notify("响应", "", responseBody); // 发送通知标题为 "响应"，内容为响应体的通知
+const requestBodyStr = $request.body;
+const responseBodyStr = $response.body;
+
+const tag = requestBody && (requestBody.operationName && requestBody.operationName == "ChatViewQuery") || (requestBody.query && requestBody.query.indexOf("ChatViewQuery") != -1;
+$notify("命中请求:" + tag);
