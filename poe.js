45a66df4eeef77c60253e9cbbe9fb1f89984760c
孙占辉 <11895200+jdukwjdo@users.noreply.github.com>
2023-04-06 13:52:18 +0800

    Update poe.js

--- poe.js
+++ poe.js
@@ -1,3 +1,14 @@
+/********************************
+
+
+[Script]
+http-request ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ script-path=https://raw.githubusercontent.com/jdukwjdo/OwnProxy/master/poe.js
+http-response ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ requires-body=1,script-path=https://raw.githubusercontent.com/jdukwjdo/OwnProxy/master/poe.js
+
+[MITM]
+hostname = www.quora.com
+********************************/
+
 const requestBody = $request.body;
 const responseBody = $response.body;
 
