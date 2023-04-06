f71e48dd5c1728327740855a741053d37a9a1ebc
孙占辉 <11895200+jdukwjdo@users.noreply.github.com>
2023-04-06 13:24:19 +0800

    Update poe.js

--- poe.js
+++ poe.js
@@ -1,8 +1,6 @@
-$notify("hello, poeInQuanx!", "", ""); // 发送通知标题为 "hello, poeInQuanx!" 的通知
-
 const requestBody = $request.body;
 const responseBody = $response.body;
 
-$notify("命中请求:", "", $request.body);
+$notify("命中请求:", "", JSON.stringify($request.body.query));
 
 $done();
