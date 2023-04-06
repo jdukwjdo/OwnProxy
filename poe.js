2c7ffbc49810375f76d609105d4972798a5e20a3
sunzhanhui1 <sunzhanhui1@jd.com>
2023-04-06 13:01:55 +0800

    用chatgpt的代码试试

--- poe.js
+++ poe.js
@@ -1,4 +1,7 @@
-console.log("hello, poeInQuanx!");
-const requestBody = $request.body;
-console.log($request.body);
-console.log($response.body);
+$notify("hello, poeInQuanx!", "", ""); // 发送通知标题为 "hello, poeInQuanx!" 的通知
+
+const requestBody = JSON.stringify($request.body); // 将请求体转换成字符串
+const responseBody = JSON.stringify($response.body); // 将响应体转换成字符串
+
+$notify("请求", "", requestBody); // 发送通知标题为 "请求"，内容为请求体的通知
+$notify("响应", "", responseBody); // 发送通知标题为 "响应"，内容为响应体的通知
