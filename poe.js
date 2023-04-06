4f4d84f8c903c4d6f374726f05624cc96e81b36f
sunzhanhui1 <sunzhanhui1@jd.com>
2023-04-06 13:21:33 +0800

    body

--- poe.js
+++ poe.js
@@ -3,7 +3,7 @@ $notify("hello, poeInQuanx!", "", ""); // 发送通知标题为 "hello, poeInQua
 const requestBody = $request.body;
 const responseBody = $response.body;
 
-const tag = requestBody.query && requestBody.query.indexOf("ChatViewQuery") != -1;
-$notify("命中请求:", "", tag);
+# const tag = $request.body.query && $request.body.query.indexOf("ChatViewQuery") != -1;
+$notify("命中请求:", "", $request.body);
 
 $done();
