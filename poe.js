b2754bb18c81ad8826c5d3527163f0bace4e3225
孙占辉 <11895200+jdukwjdo@users.noreply.github.com>
2023-04-06 13:42:09 +0800

    Update poe.js

--- poe.js
+++ poe.js
@@ -1,7 +1,7 @@
 const requestBody = $request.body;
 const responseBody = $response.body;
 
-console.log(JSON.stringify($request.body));
+console.log(JSON.stringify($request));
 console.log(JSON.stringify($response.body));
 
 $done();
