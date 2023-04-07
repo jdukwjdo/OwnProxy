384c4c3e89d252692e3d2c8b61054323269838c1
孙占辉 <11895200+jdukwjdo@users.noreply.github.com>
2023-04-07 10:13:54 +0800

    Update poe.js

--- poe.js
+++ poe.js
@@ -24,7 +24,7 @@ if('X-APOLLO-OPERATION-NAME' in $request.headers && $request.headers['X-APOLLO-O
   jdukwjdo['data']['chatOfBot']['defaultBotObject']['hasSuggestedReplies'] = false;
   jdukwjdo['data']['chatOfBot']['defaultBotObject']['allowsUserCreation'] = true;
   jdukwjdo['data']['chatOfBot']['defaultBotObject']['hasMultiplayerFeed'] = false;
-  console.log('reset poe botObject:'+ JSON.stringify($response.body));
+  console.log('reset poe botObject:'+ JSON.stringify(jdukwjdo));
   $done({body : JSON.stringify(jdukwjdo)});
 }else{
   console.log('not hit uri: ' + $request);
